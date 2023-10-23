dd39a47be4d5ef3aa494e122cd244a7d0763a60c
JoseLumbantobing <jolbntobing2@gmail.com>
2023-10-23 20:58:02 +0700

    Adding component pages and connecting them to the navbar using React Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Shop from './Pages/Shop';
+import Category from './Pages/Category';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import Login_Sign from './Pages/Login_Sign';
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Shop />} />
+          <Route path='/men' element={<Category category="men" />} />
+          <Route path='/women' element={<Category category="women" />} />
+          <Route path='/kids' element={<Category category="kids" />} />
+          <Route path='/product' element={<Product />}>
+            <Route path=':productId' element={<Product />} />
+          </Route>
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/login' element={<Login_Sign />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Pages/Login_Sign.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Login_Sign = () => {
+  return (
+    <div>Login_Sign</div>
+  )
+}
+
+export default Login_Sign
