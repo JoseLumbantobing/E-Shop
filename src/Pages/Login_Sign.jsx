502b579ccc4eefb21ed80a02ee7d70a0055d0556
JoseLumbantobing <jolbntobing2@gmail.com>
2023-10-28 23:24:13 +0700

    Fill in the program code for the Login and Sign Up page

--- src/Pages/Login_Sign.jsx
+++ src/Pages/Login_Sign.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import './CSS/Login_Sign.css'
 
 const Login_Sign = () => {
   return (
-    <div>Login_Sign</div>
+    <div className='loginsign'>
+      <div className="loginsign-container">
+        <h1>Sign Up</h1>
+        <div className="loginsign-fields">
+          <input type="text" placeholder='Your Name' />
+          <input type="email" placeholder='Email Address' />
+          <input type="password" placeholder='Password' />
+        </div>
+        <button>Continue</button>
+        <p className="loginsign-login">Already have an account? <span>Login here</span></p>
+        <div className="loginsign-agree">
+          <input type="checkbox" name='' id='' />
+          <p>By continuing, I agree to the terms of use & privacy policy.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
