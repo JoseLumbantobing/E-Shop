2b146ddc221913d903c3ffd129f8de937409fdf6
JoseLumbantobing <jolbntobing2@gmail.com>
2023-10-30 23:46:27 +0700

    Creating Product page and set function using react-router-dom

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext'
+import { useParams } from 'react-router-dom';
 
 const Product = () => {
+  //Get all product data using context (from ShopContext)
+  const {all_product} = useContext(ShopContext);
+
+  // Using params to get product id
+  const {productId} = useParams();
+
+  // variable to find a product by id from all_product
+  const product = all_product.find((e) => e.id === productId);
+
   return (
-    <div>Product</div>
+    <div></div>
   )
 }
 
